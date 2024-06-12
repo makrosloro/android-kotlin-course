66f4d43a95ddc4e52be88979ce979338a2e49b3b
Makros <lorenterodriguezmarcos@gmail.com>
2024-06-12 16:52:57 +0200

    First commit - Project initialization

---
+++ app/src/main/java/com/cursokotlin/androidkotlincourse/exercises/Exercise1.kt
@@ -0,0 +1,28 @@
+package com.cursokotlin.androidkotlincourse.exercises
+
+/**
+ * Typically, your phone provides you with a summary of notifications.
+ *
+ * In the initial code provided in the following code snippet, write a program that prints the summary message based on the number of notifications that you received. The message should include:
+ *
+ * The exact number of notifications when there are less than 100 notifications.
+ * 99+ as the number of notifications when there are 100 notifications or more.
+ */
+
+fun main() {
+    val morningNotification = 51
+    val eveningNotification = 135
+
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+
+fun printNotificationSummary(numberOfMessages: Int) {
+
+    if (numberOfMessages < 100) {
+        println("You have $numberOfMessages notifications.")
+    } else {
+        println("Your phone is blowing up! You have 99+ notifications.")
+    }
+}
---
+++ app/src/main/java/com/cursokotlin/androidkotlincourse/firstapp/FirstAppActivity.kt
@@ -0,0 +1,21 @@
+package com.cursokotlin.androidkotlincourse.firstapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.cursokotlin.androidkotlincourse.R
+
+class FirstAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_first_app)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
