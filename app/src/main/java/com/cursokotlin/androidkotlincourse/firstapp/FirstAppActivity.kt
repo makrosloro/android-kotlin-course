dc4aa1b4569b01724aea9694fb6e2262d531739e
Makros <lorenterodriguezmarcos@gmail.com>
2024-06-17 10:24:47 +0200

    FirstActivity complete

--- app/src/main/java/com/cursokotlin/androidkotlincourse/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/cursokotlin/androidkotlincourse/firstapp/FirstAppActivity.kt
@@ -1,8 +1,11 @@
 package com.cursokotlin.androidkotlincourse.firstapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.cursokotlin.androidkotlincourse.R
@@ -12,10 +15,25 @@ class FirstAppActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_first_app)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnStart = findViewById<Button>(R.id.btnStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+        btnStart.setOnClickListener {
+            val name = etName.text.toString()
+            if (name.isNotEmpty()) {
+                Log.i("MakrosDev", "Click en btnStart > $name")
+            }
+
+        }
+
+
+
     }
 }
